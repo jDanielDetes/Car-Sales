d81a1cade8392c1c8e8add05d3705bf2be19eeb0
jDanielDetes <jdanieldetes@gmail.com>
2020-01-22 21:33:15 -0500

    features showing in list

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,28 +1,24 @@
 import React from 'react';
-
+import {connect} from 'react-redux'
 
 import AddedFeature from './AddedFeature';
 
 const AddedFeatures = props => {
-  
+  console.log(props)
   return (
-    
     <div className="content">
       <h6>Added features:</h6>
-      {props.car.features.length ? (
-        <ol type="1">
-          {props.car.features.map(item => (
-            <AddedFeature key={item.id} feature={item} />
-          ))}
-        </ol>
-      ) : (
+            <AddedFeature key={props.parts.id} name={props.parts.name} />
         <p>You can purchase items from the store.</p>
-      )}
+        </div>
+        )
       
-    </div>
-  );
+   
+  
 };
 
+const mapStateToProps = (state) =>{
+  return {parts:state.added}
+}
 
-
-export default AddedFeatures
+export default connect(mapStateToProps)(AddedFeatures)
